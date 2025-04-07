f0d548aa3b40e601fae32f592ac45134ed1ab0da
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2025-04-06 22:12:11 -0300

    refactor: add cache folder on init

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func main() {
-
+	_, err := os.Stat("./.cache")
+	if err != nil {
+		os.Mkdir("./.cache", 0700)
+	}
 	http.HandleFunc("/", RequestHandler)
 	http.ListenAndServe(":3333", nil)
 }
